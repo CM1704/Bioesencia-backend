dda4079684dbd9569debb5156268696ae476ae50
CM1704 <jc71mmsc@gmail.com>
2025-07-08 01:29:42 -0600

    Implementacion de JWT

--- src/main/java/com/bioesencia/backend/config/WebConfig.java
+++ src/main/java/com/bioesencia/backend/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.bioesencia.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // puerto del frontend React
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Set-Cookie");
     }
 }
